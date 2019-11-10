97400e632dfc2448fc6b569daa4678cae63ce782
jt0118 <57547676+jt0118@users.noreply.github.com>
2019-11-10 15:24:32 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,10 +5,30 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args){
         JFrame frame = new JFrame();
-        //JFrame pane = new JFrame();
         JButton start = new JButton("Start");
         JButton end = new JButton("End");
         JButton instruction = new JButton("Instruction");
+
+        start.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        end.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
+
+        instruction.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
 
         frame.add(start);
         frame.add(end);
