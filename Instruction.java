c538aab884ba0462107c08b72637a9e0772b3dbc
hermaeusmora05 <57547664+hermaeusmora05@users.noreply.github.com>
2019-11-10 17:33:00 -0600

    Add files via upload

--- Instruction.java
+++ Instruction.java
@@ -1,11 +1,8 @@
-import java.util.*;
-import java.lang.*;
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 
 public class Instruction{
     public void showMenu() {
         JOptionPane.showMessageDialog(null, "Difficulties:\nEasy = smallest grid with least amount of mines\n\tMedium = larger grid with more mines\n\tHard = largest grid with the most amount of mines\n\nPlaying the Game:\nClick on a square to reveal it. If you think it could be a mine, place a flag.\n\tUncover all squares without hitting a mine to win the game. If you click a mine, game over!", "Need Help?", JOptionPane.INFORMATION_MESSAGE);
+
     }
 }
