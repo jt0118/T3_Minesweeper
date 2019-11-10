629764928608a52afe831853638dedade8a94498
jt0118 <57547676+jt0118@users.noreply.github.com>
2019-11-10 17:00:35 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.text.GapContent;
 
 public class Main {
     public static void main(String[] args){
@@ -25,8 +26,7 @@ public class Main {
         start.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
-                frame.setVisible(false);
-                difficultyMenu(frame);
+                SkinSetting(centerPanel, frame);
             }
         });
 
@@ -40,8 +40,7 @@ public class Main {
         instruction.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
-                Instruction help = new Instruction();
-                help.showMenu();
+
             }
         });
 
@@ -92,105 +91,49 @@ public class Main {
         northPanel.add(title);
     }
 
-    public static void difficultyMenu(JFrame aFrame){
-        // Create a frame
-        JFrame difficulty = new JFrame();
-        // Create panels : this is where the components will live
-        JPanel centerPanel = new JPanel();
-        JPanel northPanel = new JPanel();
-        JPanel eastPanel = new JPanel();
-        JPanel westPanel = new JPanel();
-        JPanel southPanel = new JPanel();
-        // Buttons
-        JButton easy = new JButton("Easy");
-        JButton medium = new JButton("Medium");
-        JButton hard = new JButton("Hard");
+    public static void SkinSetting(JPanel aPanel, JFrame aFrame){
+        aPanel.setVisible(false);
+        JPanel skinPanel = new JPanel();
+        JButton skin1 = new JButton("Skin 1");
+        JButton skin2 = new JButton("Skin 2");
         JButton back = new JButton("Back");
-        int width = 250;
-        int height = 25;
-        // Text Area
-        JLabel title = new JLabel("Choose a Difficulty");
 
-        // Button Actions
-        easy.addActionListener(new ActionListener(){
+        skin1.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
-                //send info to templateAccessor to choose easy board
-
+                aPanel.setVisible(true);
+                skinPanel.setVisible(false);
             }
         });
 
-        medium.addActionListener(new ActionListener(){
+        skin2.addActionListener(new ActionListener(){
+           @Override
+           public void actionPerformed(ActionEvent e){
+               aPanel.setVisible(true);
+               skinPanel.setVisible(false);
+           }
+        });
+
+        back.addActionListener(new ActionListener(){
             @Override
-            public void actionPerformed(ActionEvent e) {
-                //send info to templateAccessor to choose medium board
+            public void actionPerformed(ActionEvent e){
+                aPanel.setVisible(true);
+                skinPanel.setVisible(false);
             }
         });
 
-        hard.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //send info to templateAccessor to choose hard board
-            }
-        });
+        // Skin Panel Settings
+        skinPanel.setVisible(true);
+        skinPanel.setLayout(new GridLayout(3,1, 10, 10));
+        skinPanel.setPreferredSize(new Dimension(250, 200));
 
-        back.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                difficulty.setVisible(false);
-                aFrame.setVisible(true);
-            }
-        });
+        // Add skin panel to frame
+        aFrame.add(skinPanel, BorderLayout.CENTER);
 
-        // Button settings
-        easy.setPreferredSize(new Dimension(width, height));
-        medium.setPreferredSize(new Dimension(width, height));
-        hard.setPreferredSize(new Dimension(width, height));
-        back.setPreferredSize(new Dimension(width, height));
-        // centerPanel settings
-        centerPanel.setVisible(true);
-        centerPanel.setLayout(new GridLayout(3,1, 10, 10));
-        centerPanel.setPreferredSize(new Dimension(width, 200));
-
-        // eastPanel settings
-        eastPanel.setVisible(true);
-        eastPanel.setLayout(new GridLayout(3,1, 10, 10));
-        eastPanel.setPreferredSize(new Dimension(100, 15));
-
-        // westPanel settings
-        westPanel.setVisible(true);
-        westPanel.setPreferredSize(new Dimension(100, 15));
-
-        // northPanel settings
-        northPanel.setVisible(true);
-        northPanel.setPreferredSize(new Dimension(500, 50));
-
-        // southPanel settings
-        southPanel.setVisible(true);
-        southPanel.setPreferredSize(new Dimension(500, 50));
-
-        // frame settings
-        difficulty.setLayout(new BorderLayout());
-        difficulty.setSize(500,500);
-        difficulty.setVisible(true);
-        difficulty.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Adding components to frame
-        difficulty.add(centerPanel, BorderLayout.CENTER);
-        difficulty.add(northPanel, BorderLayout.NORTH);
-        difficulty.add(eastPanel, BorderLayout.EAST);
-        difficulty.add(westPanel, BorderLayout.WEST);
-        difficulty.add(southPanel, BorderLayout.SOUTH);
-
-        // Adding components to the panels
-        centerPanel.add(easy);
-        centerPanel.add(medium);
-        centerPanel.add(hard);
-        centerPanel.add(back);
-        northPanel.add(title);
+        // Add skin selection to panel
+        skinPanel.add(skin1);
+        skinPanel.add(skin2);
+        skinPanel.add(back);
     }
 
-
 }
-
-
